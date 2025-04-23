60e8f649c4f9d9a67398962ca459c9d0d7733c35
Alexssander <alexssander.f.almeida2006@gmail.com>
2025-04-23 12:01:40 -0300

    Criando a classe posição do tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace xadrez_console 
+﻿using tabuleiro;
+namespace xadrez_console 
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Posicao P = new Posicao(3, 4);
+            Console.WriteLine(P);
         }
     }
 }
