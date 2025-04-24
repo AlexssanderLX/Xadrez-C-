89a97c879b7aa8a1d858de50f0c55543aa94a2dc
Alexssander <alexssander.f.almeida2006@gmail.com>
2025-04-24 11:52:16 -0300

    Criando a classe de posição do Xadrez

--- Program.cs
+++ Program.cs
@@ -7,19 +7,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-                Tela.imprimirtabuleiro(tab);
-                Console.WriteLine();
-            }
-            catch (TabuleiroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            PosicaoXadrez posx = new PosicaoXadrez('a', 1);
+            Console.WriteLine(posx);
+            Console.WriteLine(posx.toPosicao());
         }
     }
 }
