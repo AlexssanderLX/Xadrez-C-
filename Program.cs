24cd8307cb11054676286d63d6527afc20b2c8ba
Alexssander <alexssander.f.almeida2006@gmail.com>
2025-04-24 11:28:28 -0300

    Tratando excessões personalizadas 2

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 ﻿using tabuleiro;
 using Xadrez_Console;
 using Xadrez;
-namespace xadrez_console 
+namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-            Tela.imprimirtabuleiro(tab);
-            Console.WriteLine();
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                Tela.imprimirtabuleiro(tab);
+                Console.WriteLine();
+            }
+            catch (TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
